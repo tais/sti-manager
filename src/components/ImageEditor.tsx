3a50c4172c6e77953948fa3bca13585480489ef9
tais <378873+tais@users.noreply.github.com>
2025-08-30 13:30:16 +0200

    Fix build issue

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { EditableImage, EditableStiFile } from '../types/sti';
 import { StiEditingApi } from '../services/api';
 import './ImageEditor.css';
